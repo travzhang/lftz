10677fd09f0da6a32aef4b167ae3790584cd9d7a
Travis Zhang <wr.zhang25@gmail.com>
2025-11-05 17:13:33 +0800

    chore: daily development

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { json } from 'express';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 // dotenv.config({
 //   path: path.resolve(__dirname, '../../../.env'),
